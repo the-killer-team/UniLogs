31eb217f2e18603abb6eb261ef62c4b07c88631e
Mata morad <Mata@Untameables-MacBook-Pro.local>
2019-05-07 14:57:19 +0200

    Header Barre navigation
    Barre navigation avec la barre de recherche le bouton ajouter et l'icone alarme..
    Barre de recherche pas programée il manque des élements.

---
+++ ulogs/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+    <title>ULOGS</title>
+</head>
+<body>
+<header>
+    
+<div id="backheader">
+<nav class="navbar navbar-light ">
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Rechercher" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rechercher</button>
+  </form>
+</nav>
+ 
+     <img id="iconealarme" src="images/iconealarme.png" alt="icone d'une alarme">
+ <img id="iconeplus" src="images/iconeplus.png" alt="icone d'une alarme">
+    
+
+
+
+</div>
+    
+</header>
+
+
+    
+</body>
+</html>
